f76cf6e8aef3ddf2001848d829d8c85d54bfd6f0
samuelwondimu <wsamuelwondimu@gmail.com>
2023-08-16 12:49:04 +0300

    style: add font family to _app mainfile

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -2,5 +2,9 @@ import type { AppProps } from 'next/app';
 import '@ui/styles/globals.css';
 
 export default function Docs({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main lang="en" className="h-full text-base antialiased">
+      <Component {...pageProps} />
+    </main>
+  );
 }
