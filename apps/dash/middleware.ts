b5dd7d7e4da2a4efd88e12d6e8c391bfb90a64c4
Sam Rizvi <srizvi@users.noreply.github.com>
2023-08-21 23:08:21 -0700

    chore(linting): modified linting and middleware
    - chore(.eslintignore): update .eslintignore to ignore generated files in apps/dash/.wundergraph/generated/ directory
    - chore(.gitignore): update .gitignore to ignore generated files in **/.wundergraph/generated/ directory
    - chore(.gitignore): remove unused entries related to .pnp and .wundergraph/cache
    - chore(.gitignore): update .gitignore to ignore .env, .env.local, .env.production, and .env.development files
    - chore(apps/dash/.gitignore): remove unused entries related to .pnp and .wundergraph/cache
    - chore(apps/dash/.gitignore): update .gitignore to ignore .env, .env.local, .env.production, and .env.development files
    - chore(apps/dash/lib/handleInngest.ts): remove console.log statement
    - chore(apps/dash/middleware.ts): update publicRoutes in authMiddleware configuration
    
    Review and Update CRUD operations #48

--- apps/dash/middleware.ts
+++ apps/dash/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/sign-in', '/sign-up', '/api/webhook'],
+  publicRoutes: ['/', '/sign-in', '/sign-up', '/api/webhook'],
   ignoredRoutes: ['/api/inngest'],
 });
 
