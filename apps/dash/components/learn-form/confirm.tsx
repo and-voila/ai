42f5e2a07021da8abbdaa25eb7a9a2281c3139d2
samuelwondimu <wsamuelwondimu@gmail.com>
2023-08-23 19:22:03 +0300

    * feat(confirm.tsx): add ConfirmComponent to learn-form directory

---
+++ apps/dash/components/learn-form/confirm.tsx
@@ -0,0 +1,62 @@
+import { useAuth } from '@clerk/nextjs';
+import { Message } from 'ai';
+import React, { Dispatch, SetStateAction } from 'react';
+import { Button } from 'ui';
+
+import { WritingStyleType } from '@/inngest/functions';
+import { getUserWrittingRedis } from '@/lib/handleInngest';
+
+type ResponseRedis = {
+  status: 'pending' | 'completed';
+  writtingAnalysis: WritingStyleType;
+  messages?: {
+    id: string;
+    role: 'system' | 'user';
+    content: string;
+  }[];
+};
+
+const ConfirmComponent = ({
+  setStep,
+  setLearnMessages,
+}: {
+  setStep: Dispatch<SetStateAction<number>>;
+  setLearnMessages: Dispatch<SetStateAction<Message[]>>;
+}) => {
+  const { userId } = useAuth();
+  async function analyzedSample() {
+    let response: ResponseRedis | null = null;
+
+    do {
+      const res = await getUserWrittingRedis(userId);
+      response = res; // Update the response based on API result
+
+      if (response?.status === 'pending') {
+        await new Promise((resolve) => setTimeout(resolve, 10000));
+      }
+    } while (response?.status === 'pending');
+
+    if (response) {
+      setLearnMessages((prevMessages) => [
+        ...prevMessages,
+        ...response.messages!,
+      ]);
+      setStep((prevStep) => prevStep + 1);
+    }
+  }
+
+  return (
+    <div>
+      <Button
+        className="text-primary-background rounded-md bg-primary-foreground px-4 py-2"
+        onClick={() => {
+          analyzedSample();
+        }}
+      >
+        hey
+      </Button>
+    </div>
+  );
+};
+
+export default ConfirmComponent;
