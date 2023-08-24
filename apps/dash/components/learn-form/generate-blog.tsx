fad78ea457553d2099af006621f746f3dc22c0ca
samuelwondimu <wsamuelwondimu@gmail.com>
2023-08-24 13:32:44 +0300

    * fix(generate-blog.tsx): remove unused setStep variable * feat(generate-blog.tsx): add setLoading state and set it to true during analyzedSample function * fix(generate-blog.tsx): set setLoading state to false after analyzedSample function

--- apps/dash/components/learn-form/generate-blog.tsx
+++ apps/dash/components/learn-form/generate-blog.tsx
@@ -12,7 +12,6 @@ import {
 
 const TOPIC_ONE = `
 Create a well-structured blog post of 750 words or less on the topic: "Empowering Creators with AI: A New Dawn of Possibilities". The blog post should discuss the following:
-
 1. Introduction: Brief overview of AI and its relevance in today's world.
 2. Body: Explain how AI is empowering creators, providing examples of AI tools and platforms that creators are using to enhance their creativity and efficiency. Discuss the impact of AI on different domains of creativity like art, music, writing, and more.
 3. Conclusion: Forecast on the future implications of AI for creators and how it will revolutionize creativity.
@@ -44,16 +43,17 @@ type ResponseRedis = {
 };
 
 function GenerateBlog({
-  setStep,
   setLearnMessages,
+  setLoading,
 }: {
-  setStep: Dispatch<SetStateAction<number>>;
   setLearnMessages: Dispatch<SetStateAction<Message[]>>;
+  setLoading: Dispatch<SetStateAction<boolean>>;
 }) {
   const { userId } = useAuth();
 
   async function analyzedSample() {
     let response: ResponseRedis | null = null;
+    setLoading(true);
     await new Promise((resolve) => setTimeout(resolve, 10000));
 
     do {
@@ -72,8 +72,8 @@ function GenerateBlog({
         ...prevMessages,
         ...response.messages!,
       ]);
-      setStep((prevStep) => prevStep + 1);
     }
+    setLoading(false);
   }
 
   async function handlegenerate() {
@@ -85,14 +85,15 @@ function GenerateBlog({
   }
 
   return (
-    <div className="mx-auto block grid max-w-2xl grid-cols-3 gap-4 px-4 py-4 lg:px-16">
+    <div className="mb-10 block grid w-full grid-cols-3 gap-4">
       {generateOptions.map((option) => (
         <Button
           key={option.event}
-          className="cursor-pointer rounded-md border bg-muted p-4 text-center hover:animate-none hover:bg-primary hover:text-white"
+          className="p-8 text-2xl"
           onClick={async () => {
             await handlegenerate();
           }}
+          disabled={true}
         >
           {option.label}
         </Button>
