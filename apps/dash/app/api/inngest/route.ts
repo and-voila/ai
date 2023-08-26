118c719b14faa93c969a63fda1780d70c1a08ace
Sam Rizvi <srizvi@users.noreply.github.com>
2023-08-25 23:36:06 -0700

    chore: remove-wonky-learn-prototype #114
    - remove api routes for chat and inggest

--- apps/dash/app/api/inngest/route.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from 'inngest/next';
-
-import { createWritingAnalysis, inngest } from '@/inngest/functions';
-
-export const runtime = 'edge';
-
-export const { GET, POST, PUT } = serve(inngest, [createWritingAnalysis], {
-  signingKey: process.env.INNGEST_SIGNING_KEY,
-});
